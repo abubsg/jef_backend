431b59de3e6c22f94c020f8fd2dc2df1a9b522b6
Abubakar Garba <abubsg@gmail.com>
2022-12-08 14:30:30 +0100

    worked on the app config aspect for the home page

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,33 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
-  if (!token) return res.status(401).send("ACCESS DENIED. NO TOKEN AVAILABLE");
+  // grab token from cookie
+  const { token } = req.cookies;
+
+  // if no token stop
+  if (!token) {
+    return res.status(403).send("not authorized, please log in");
+  }
+
+  // else decode token and get id
   try {
     const decode = jwt.verify(token, process.env.jwtPrivateKey);
     req.user = decode;
-    next();
   } catch (error) {
-    res.status(400).send("Invalid Token");
+    res.status(400).send("Invalid token");
   }
+
+  return next();
 };
+
+// module.exports = function (req, res, next) {
+//   const token = req.header("x-auth-token");
+//   if (!token) return res.status(401).send("ACCESS DENIED. NO TOKEN AVAILABLE");
+//   try {
+//     const decode = jwt.verify(token, process.env.jwtPrivateKey);
+//     req.user = decode;
+//     next();
+//   } catch (error) {
+//     res.status(400).send("Invalid Token");
+//   }
+// };
