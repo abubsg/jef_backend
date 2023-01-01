ea5066ff820ab0954f87478c596ccfd2ddfc55f4
Abubakar Garba <abubsg@gmail.com>
2023-01-01 12:35:04 +0100

    added donations End points

--- models/donations.model.js
+++ models/donations.model.js
@@ -1,22 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const sponsorshipSchema = new Schema(
+const donationsSchema = new Schema(
   {
     donorFirst_name: {
       type: String,
-      required: true,
       minLength: 3,
       maxLength: 50,
     },
     donorLast_name: {
       type: String,
-      required: true,
       minLength: 3,
       maxLength: 50,
     },
     donorEmail: {
-      required: true,
       type: String,
       maxLength: 50,
       minLength: 3,
@@ -34,10 +31,10 @@ const sponsorshipSchema = new Schema(
       required: true,
     },
     amount: { type: Number, required: true },
-    recieptient: { type: Schema.Types.ObjectId, ref: "Donee" },
-    purpose: { type: String },
+    recieptient: { type: String, default: null },
+    purpose: { type: String, required: true, enum: ["donation", "sponsorship"] },
   },
   { timestamps: true }
 );
 
-module.exports = Sponsorships = mongoose.model("sponsorship", sponsorshipSchema);
+module.exports = Donations = mongoose.model("donations", donationsSchema);
