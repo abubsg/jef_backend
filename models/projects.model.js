5f49434a8af4abf7211aa0f0d591269041933cae
Abubakar Garba <abubsg@gmail.com>
2023-01-08 12:19:23 +0100

    added project route

---
+++ models/projects.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const projectsSchema = new Schema(
+  {
+    postedBy: { type: Schema.Types.ObjectId, ref: "User" },
+    description: { type: String },
+    address: { type: String },
+    title: { type: String, required: true },
+    upload: [Object],
+    startDate: { type: Date },
+    endDate: { type: Date },
+  },
+  { timestamps: true }
+);
+
+module.exports = Projects = mongoose.model("project", projectsSchema);
