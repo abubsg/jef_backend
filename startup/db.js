d38d61f2ce6ba284c2cf9a70f3f77bf2c10a6b67
Hassan Usman <husmanjah@gmail.com>
2023-01-17 08:21:13 +0100

    fix error

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,7 @@ const logger = require("./logging");
 module.exports = function () {
   const uri_local = "mongodb://127.0.0.1:27017/jef";
   const uri_online = process.env.MONGO_URI;
+  mongoose.set("strictQuery", true);
   let uri;
   if (process.env.NODE_ENV === "production") {
     uri = uri_online;
